7822eb51ec11adbc998ec865556bcafb74c04260
BuilderBoiXx1335 <Workatapizzaplace11@gmail.com>
2025-09-10 14:18:18 -0400

    Update cloakMode.js

--- cloakMode.js
+++ cloakMode.js
@@ -1,1 +1,7 @@
+(function () {
+  if (localStorage.getItem("relayDetected") === "true") {
+    document.title = "Math Homework - Google Docs";
+    document.body.innerHTML = "<h1>Loading Google Docs...</h1>";
+  }
+})();
 
